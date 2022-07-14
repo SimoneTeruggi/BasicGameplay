b1ed0fea60c4e506178d78cc3b1944f6c9ae8b3d
Simone <simone.teruggi@polimi.it>
2022-07-14 21:57:24 +0200

    challange 2 completed

---
+++ Callange2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float spamInterval = 3.0f;
+    private float timeKeyAllowed;
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog (check that 3 sec passed before allowing to press again)
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > timeKeyAllowed)
+            {
+                float timeFromKeyPressed = Time.time;
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                timeKeyAllowed = Time.time + spamInterval;
+            }
+        }
+}
