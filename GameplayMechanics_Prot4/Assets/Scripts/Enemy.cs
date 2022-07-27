03a728ab5e4e2d5743ef3dc8fad23ce4d0eb5899
Simone <simone.teruggi@polimi.it>
2022-07-27 11:05:14 +0200

    Prototype 4 expert add on DONE

--- GameplayMechanics_Prot4/Assets/Scripts/Enemy.cs
+++ GameplayMechanics_Prot4/Assets/Scripts/Enemy.cs
@@ -12,12 +12,26 @@ public class Enemy : MonoBehaviour
     private PlayerController playerControllerScript;
 
     public float lowerBound = -10;
+
+    public bool isBoss = false;
+
+    public float spawnInterval;
+    private float nextSpawn;
+
+    public int miniEnemySpawnCount;
+
+    private SpawnManager spawnManager;
     
     // Start is called before the first frame update
     void Start()
     {
         enemyRb = GetComponent<Rigidbody>();
         player = GameObject.Find("Player");
+
+        if (isBoss)
+        {
+            spawnManager = FindObjectOfType<SpawnManager>();
+        }
 
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
@@ -30,11 +44,21 @@ public class Enemy : MonoBehaviour
             Vector3 lookDirection = (player.transform.position - transform.position).normalized;
             enemyRb.AddForce(lookDirection * speed);
 
-            if (gameObject.transform.position.y < lowerBound)
+            if (isBoss)
             {
-                Destroy(gameObject);
+                if (Time.time > nextSpawn)
+                {
+                    nextSpawn = Time.time + spawnInterval;
+                    spawnManager.SpawnMiniEnemy(miniEnemySpawnCount);
+                }
             }
+
         }
-        
+
+        if (gameObject.transform.position.y < lowerBound)
+        {
+            Destroy(gameObject);
+        }
+
     }
 }
--- GameplayMechanics_Prot4/Assets/Scripts/SpawnManager.cs
+++ GameplayMechanics_Prot4/Assets/Scripts/SpawnManager.cs
@@ -13,6 +13,10 @@ public class SpawnManager : MonoBehaviour
     public int enemycount = 0;
     public int waveNumber = 1;
 
+    public GameObject bossPrefab;
+    public GameObject[] miniEnemyPrefab;
+    public int bossRound;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,10 +30,21 @@ public class SpawnManager : MonoBehaviour
     void Update()
     {
         enemycount = FindObjectsOfType<Enemy>().Length;
+
         if (enemycount == 0 && !playerControllerScript.isGameOver)
         {
             waveNumber++;
-            SpawnEnemyWave(waveNumber);
+
+            //Spawn a boss every x number of waves
+            if (waveNumber % bossRound == 0)
+            {
+                SpawnBossWave(waveNumber);
+            }
+            else
+            {
+                SpawnEnemyWave(waveNumber);
+            }
+            
             int randomPowerUp = Random.Range(0, powerUps.Length);
             Instantiate(powerUps[randomPowerUp], GenerateSpawnPosition(), powerUps[randomPowerUp].transform.rotation);
         }
@@ -43,6 +58,33 @@ public class SpawnManager : MonoBehaviour
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
 
         return randomPos;
+    }
+
+    void SpawnBossWave(int currentRound)
+    {
+        int miniEnemysToSpawn;
+        //We don't want to divide by 0!
+        if (bossRound != 0)
+        {
+            miniEnemysToSpawn = currentRound / bossRound;
+        }
+        else
+        {
+            miniEnemysToSpawn = 1;
+        }
+
+        var boss = Instantiate(bossPrefab, GenerateSpawnPosition(), bossPrefab.transform.rotation);
+        boss.GetComponent<Enemy>().miniEnemySpawnCount = miniEnemysToSpawn;
+    }
+
+    public void SpawnMiniEnemy(int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            int randomMini = Random.Range(0, miniEnemyPrefab.Length);
+            Instantiate(miniEnemyPrefab[randomMini], GenerateSpawnPosition(), miniEnemyPrefab[randomMini].transform.rotation);
+
+        }
     }
 
     void SpawnEnemyWave(int enemyToSpawn)
